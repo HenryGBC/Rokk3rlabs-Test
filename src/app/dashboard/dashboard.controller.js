ecb2128a02c39df47c9ff64c1bef92ba041f7ab0
HenryGBC <henry.bravo89@gmail.com>
2017-01-16 13:27:16 -0430

    Dashboard controller

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -6,15 +6,16 @@
     .controller('DashboardController', DashboardController);
 
   /** @ngInject */
-  function DashboardController($timeout) {
+  function DashboardController($timeout, $state) {
     var vm = this;
 
 
     activate();
-    
+
     function activate() {
       vm.hideBar = false;
       console.log('active');
+      $state.go('dashboard.analytics');
     }
 
   }
